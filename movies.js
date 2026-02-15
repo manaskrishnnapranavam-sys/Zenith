a9fbf8e32474c330c2b0237d5c94df5da7a5b0b6
manaskrishnnapranavam-sys <manaskrishnnapranavam@gmail.com>
2026-02-15 13:10:28 +0530

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,24 @@
+const movieData = [
+    {
+        "id": "ZEN-1771140879988",
+        "title": "THE FIRST RIDE",
+        "genre": "Tech-Thriller | Industrial Noir | Action Drama",
+        "marketingTag": "A SYSTEM BREACH",
+        "plot": "Efficiency is the only law until a 12-second power reallocation leads to a fatal collapse. Navaneeth and Jerome find themselves caught in a terrifying, symmetrical synchronization—two variables the grid failed to account for.  Hunted through industrial backlanes by the cold, methodical Sinan and the visor-clad Manas, the duo must weaponize their instincts to survive. In a world of mechanical warfare, their first ride is a high-speed descent into the system's core.",
+        "videoFile": "https://drive.google.com/file/d/1xkamfBsxbQ5h4VFdg2xuUVMJtP18f1v3/view?usp=sharing",
+        "posterLandscape": "https://i.postimg.cc/NfBXW2Zz/PS-169.jpg",
+        "posterPortrait": "https://i.postimg.cc/dQmkmtLM/Chat-GPT-Image-Feb-15-2026-01-02-37-PM.png",
+        "isFeatured": true
+    },
+    {
+        "id": "ZEN-1771141086996",
+        "title": "THE ASCENSION: DAY ONE",
+        "genre": "Sci-Fi | Action-Thriller",
+        "marketingTag": "A SOLAR EVENT",
+        "plot": "In the blinding, marble-white metropolis of Veridia, Samuel is a \"face designed to be forgotten\"—until a routine delivery goes nuclear. When a prototype Solar Core explodes and fuses with his body, the unremarkable courier becomes the city's most wanted \"stolen asset.\" Hunted by the cold-blooded Curator and a squad of high-tech mercenaries, Samuel discovers the golden circuitry pulsing beneath his skin isn't just a glitch—it’s an evolution. Cornered atop a skyscraper at sunset, he takes a leap of faith that triggers a massive mid-air transformation into a 20-foot mechanical Titan. The hunt has ended, but for Samuel, the war is only on Day One.",
+        "videoFile": "https://drive.google.com/file/d/1PFmqXoSTeVIxz6vWb4JMXKxsHLSMXbf0/view?usp=sharing",
+        "posterLandscape": "https://i.postimg.cc/hGQYdY4t/a-wide-cinematic-anime-movie-poster-in-l-uppf-Acpp-SQSp-RFy-Iw-BYBz-Q-AWo-S5h-XFSVWnmvag-Lw6tk-A-sd.jpg",
+        "posterPortrait": "https://i.postimg.cc/bNryDznG/poster.jpg",
+        "isFeatured": true
+    }
+];
