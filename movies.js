c65df82852bacf4091456091186acb2a733ae19b
manaskrishnnapranavam-sys <manaskrishnnapranavam@gmail.com>
2026-02-15 11:55:51 +0530

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,13 @@
+const movieData = [
+    {
+        "id": "ZEN-1771136704170",
+        "title": "THE ASCENSION: DAY ONE",
+        "genre": "Sci-Fi, Action-Thriller",
+        "marketingTag": "UPCOMING",
+        "plot": "In the blinding, marble-white metropolis of Veridia, Samuel is a \"face designed to be forgotten\"—until a routine delivery goes nuclear. When a prototype Solar Core explodes and fuses with his body, the unremarkable courier becomes the city's most wanted \"stolen asset\". Hunted by the cold-blooded Curator and a squad of high-tech mercenaries, Samuel discovers the golden circuitry pulsing beneath his skin isn't just a glitch—it’s an evolution. Cornered atop a skyscraper at sunset, he takes a leap of faith that triggers a massive mid-air transformation into a 20-foot mechanical Titan. The hunt has ended, but for Samuel, the war is only on Day One.\n",
+        "videoFile": "https://drive.google.com/file/d/1PFmqXoSTeVIxz6vWb4JMXKxsHLSMXbf0/view?usp=sharing",
+        "posterLandscape": "https://i.postimg.cc/hGQYdY4t/a-wide-cinematic-anime-movie-poster-in-l-uppf-Acpp-SQSp-RFy-Iw-BYBz-Q-AWo-S5h-XFSVWnmvag-Lw6tk-A-sd.jpg",
+        "posterPortrait": "https://i.postimg.cc/d1yKJ1Zd/poster.png",
+        "isFeatured": true
+    }
+];
